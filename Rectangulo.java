6088d6e3e887393853e6d8d0f40b5ea333530971
juan <juanillo9121993@gmail.com>
2020-05-27 15:32:39 +0200

    Creacion de la clase Rectangulo y el metodo obtenerArea

---
+++ Rectangulo.java
@@ -0,0 +1,21 @@
+
+package figuras;
+
+/**
+ *
+ * @author juan4
+ */
+public class Rectangulo {
+    private double lado1;
+    private double lado2;
+    
+    public Rectangulo(double l1, double l2){
+        lado1 = l1;
+        lado2 = l2;
+    }
+    
+    public double obtenerArea(){
+        return lado1 * lado2;
+    }
+    
+}
